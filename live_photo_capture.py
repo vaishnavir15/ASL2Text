13f3ed1c388ee2e5e28b81716a4e84ea6ba49e0a
Vaishnavi Ratnasabapathy <vratnasa@uwaterloo.ca>
2023-12-22 22:58:43 -0500

    saves a close up image of the hand w the landmarks

--- live_photo_capture.py
+++ live_photo_capture.py
@@ -1,42 +1,73 @@
 import cv2
 import mediapipe as mp
-import numpy as np
 import time
+import numpy as np
 
 # Time Tracking
 startCamera = time.time()
 
-
-
 # Open the camera
 cap = cv2.VideoCapture(0)
+
+# Initialize Mediapipe Hands
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1)  # Set max_num_hands to 1
+
+# Variable to store the last detected hand landmarks
+last_hand_landmarks = None
 
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Display the resulting frame
+    # Convert the BGR image to RGB
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Process the frame with Mediapipe Hands
+    results = hands.process(rgb_frame)
+
+    # Check if hands are detected
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Draw landmarks on the frame
+            mp.solutions.drawing_utils.draw_landmarks(
+                frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+            # Store the detected hand landmarks
+            last_hand_landmarks = hand_landmarks
+
+    # Display the resulting frame with landmarks
     cv2.imshow('ASL to Text Translator', frame)
 
     # Check for key press
-    key = cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(1)
 
-    # Capture and process the close-up image when the spacebar is pressed
-    if key == ord(' '):
-        # Get the bounding box around the hand (full frame in this case)
-        bbox = [0, 0, frame.shape[1], frame.shape[0]]
-        
-        # Extract the region of interest (ROI)
-        hand_roi = frame[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]]
+    # Capture and save the close-up image when the spacebar is pressed
+    if key == 32:  # 32 is the ASCII code for spacebar
+        print("Spacebar pressed")
+        if last_hand_landmarks is not None:
+            # Calculate bounding box based on landmarks
+            x = int(min(last_hand_landmarks.landmark, key=lambda l: l.x).x * frame.shape[1])
+            y = int(min(last_hand_landmarks.landmark, key=lambda l: l.y).y * frame.shape[0])
+            w = int((max(last_hand_landmarks.landmark, key=lambda l: l.x).x - min(last_hand_landmarks.landmark, key=lambda l: l.x).x) * frame.shape[1])
+            h = int((max(last_hand_landmarks.landmark, key=lambda l: l.y).y - min(last_hand_landmarks.landmark, key=lambda l: l.y).y) * frame.shape[0])
 
-        # Make the background black
-        hand_roi[np.where((hand_roi == [0, 0, 0]).all(axis=2))] = [0, 0, 0]
+            # Ensure the bounding box is within the image boundaries
+            x = max(0, x)
+            y = max(0, y)
+            w = min(frame.shape[1], x + w)
+            h = min(frame.shape[0], y + h)
 
-        # Display the close-up image
-        cv2.imshow('Close-Up Hand Image', hand_roi)
+            # Extract the region of interest (ROI) around the hand
+            hand_roi = frame[y:h, x:w]
 
-        # Save the close-up image with a black background
-        cv2.imwrite('hand_closeup.png', hand_roi)
+            # Save the close-up image with landmarks
+            cv2.imwrite('hand_with_landmarks.png', hand_roi)
+
+            # Display the close-up image
+            cv2.imshow('Close-Up Hand Image', hand_roi)
+
+            print("Image saved")
 
     # Break the loop when 'q' is pressed
     elif key == ord('q'):
@@ -45,9 +76,6 @@ while True:
 # Release the camera and close all windows
 cap.release()
 cv2.destroyAllWindows()
-
-
-
 
 # Time Tracking
 endCamera = time.time()
