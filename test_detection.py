8af43b40293f0fdce16cf8b7936e9330fca89c97
Keta <k3khatri@uwaterloo.ca>
2023-12-27 23:44:18 -0700

    added abcdef black images

--- test_detection.py
+++ test_detection.py
@@ -5,34 +5,43 @@ import os
 import time
 
 startDetection = time.time()
+
 # Load the trained model
 model = load_model('als_model.h5')
 
-test_image_path = 'images/hand_without_landmarks-PhotoRoom.png-PhotoRoom.png'  # Replace with the path to your test image 
-img_size = (400, 400)
+# Path to the folder containing test images
+images_folder = 'images'
 
-test_img = cv2.imread(test_image_path)
-print(test_img)
-if test_img is None:
-    print(f"Error loading image from path: {test_image_path}")
-else:
-    print("Image loaded successfully.")
-    test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
-    test_img = cv2.resize(test_img, img_size)
-    test_img = test_img / 255.0  # Normalize pixel values to be between 0 and 1
-    test_img = np.expand_dims(test_img, axis=0)  # Add batch dimension
+# Iterate through each file in the folder
+for filename in os.listdir(images_folder):
+    if filename.endswith(('.png', '.jpg', '.jpeg')):
+        # Construct the full path to the image file
+        test_image_path = os.path.join(images_folder, filename)
 
-    # Make predictions
-    predictions = model.predict(test_img)
+        print(f"\nTesting image: {test_image_path}")
 
-    # Assuming it's a classification task, get the predicted class
-    predicted_class = np.argmax(predictions)
+        # Load and preprocess the test image
+        test_img = cv2.imread(test_image_path)
 
-    # Print or use the predicted class as needed
-    print(f"Predicted Class: {predicted_class}")
+        if test_img is None:
+            print(f"Error loading image from path: {test_image_path}")
+        else:
+            print("Image loaded successfully.")
+            test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+            test_img = cv2.resize(test_img, (400, 400))
+            test_img = test_img / 255.0  # Normalize pixel values to be between 0 and 1
+            test_img = np.expand_dims(test_img, axis=0)  # Add batch dimension
+
+            # Make predictions
+            predictions = model.predict(test_img)
+
+            # Assuming it's a classification task, get the predicted class
+            predicted_class = np.argmax(predictions)
+
+            # Print or use the predicted class as needed
+            print(f"Predicted Class: {predicted_class}")
 
 endDetection = time.time()
 totalDetection = endDetection - startDetection
-print(f"Time taken to train model: {totalDetection} seconds")
+print(f"Time taken to test images: {totalDetection} seconds")
 print("DONE")
-
